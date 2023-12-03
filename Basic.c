e12f4926308bd6beb390507172bafac59658b2a3
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2023-12-03 19:46:50 +0100

    Actually added working code and comments to create and write into the file

--- Basic.c
+++ Basic.c
@@ -2,6 +2,14 @@
 int main(){
     FILE *player;
     player=fopen("first.txt", "w");
-    //This line will create a blank text file called first.
+    //We have created a file pointer and assigned it to a file called first.txt
+    //This line will create a blank text file called first.txt in the same directory as the program in write mode
+    fputs("Hello world. ", player);
+    //This line will write the string "Hello world" in the file first.txt
+    fputs("I am learning file handling in C programming language\n", player);
+    //So technically, it works simil,ar to printf function.
+    fprintf(player,"%s",  "I am learning file handling in C programming language using the fprintf function");
+    fclose(player);
 
+    return 0;
 }
